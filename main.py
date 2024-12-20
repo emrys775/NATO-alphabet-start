7de1f82b8fd492c503a51437893ba021650ab2e7
emrys77 <emrys775@gmail.com>
2024-12-20 12:29:12 +0000

    Remove redundant TODO comments from main.py
    The unnecessary TODO comments were removed to clean up the code and improve readability. These tasks appear to be completed or no longer relevant, making the comments obsolete.

--- main.py
+++ main.py
@@ -35,8 +35,4 @@ print(phonetic_code_words)
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-#{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
